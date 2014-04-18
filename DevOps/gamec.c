1360172c07188bab4218cadfc5788ec3d81942ae
James Wen <jrw2175@columbia.edu>
2014-04-18 13:02:51 -0400

    * operator in bash automatically gets replaced by regex matches argument-wise apparently...

--- DevOps/gamec.c
+++ DevOps/gamec.c
@@ -5,40 +5,37 @@
 #include <string.h>
 
 int main(int argc, char **argv){
+
   //Add argument length check
   //Maybe add check for .game extension later?
   if(argc < 2){
-    printf("\nTo compile your .game file, please use the following command:\n\n%s [.game file] ...\n\n or \n\n %s *.game\n\nPlease note that you can only compile one .game file at a time.\n", argv[0], argv[0]);
+    printf("\nTo compile your .game file, please use the following command:\n\n%s [.game file] ...\n\n or \n\n %s *.game\n\n", argv[0], argv[0]);
     exit(1);
   }
+  int mv_flag = 0;
+  int i = 1;
+  char mv_directory[100];
+  while(!mv_flag && i < argc){
+    if(strcmp(argv[i], "-m") == 0){
+      mv_flag = 1; 
+      sprintf(mv_directory, "%s", argv[++i]);
+    }
+    i++;
+  }
 
-  if(strcmp(argv[1], "*.game")){
-    //compile all .game in current directory
-    glob_t data;
-    switch( glob("*.game", 0, NULL, &data) ){
-      case 0:
-        break;
-      case GLOB_NOSPACE:
-        break;
-      case GLOB_ABORTED:
-        break;
-      case GLOB_NOMATCH:
-        printf("No .game files found.\n");
-        break;
-      default:
-        break;
+  char compiled[100];
+
+    int k = 1;
+    for(k= 1; k < argc; k++){ 
+      if(strstr(argv[k], ".game")){
+        printf("python game.py %s\n", argv[k]);
+        printf("%d\n", k);
+        //execlp("python", "python", "game.py", argv[k]);
+        if(mv_flag){
+          sprintf(compiled, "%s.py", argv[k]);
+          execlp("mv", "mv", compiled, mv_directory);
+        }
+      }
     }
-    int j;
-    for(j = 0; j < data.gl_pathc; j++){
-      execlp("python", "python", "game.py", data.gl_pathv[j]); 
-    }
-    globfree(&data);  
-  }
-  else{
-    int i;
-    for(i= 1; i < argc; i++){
-      execlp("python", "python", "game.py", argv[i]);
-    }
-  }
   return 0;
 }

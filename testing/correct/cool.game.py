bf260b8acf9a83fe6be1996605477280bf343881
James Wen <jrw2175@columbia.edu>
2014-04-18 15:07:16 -0400

    Removed old testing framework

--- testing/correct/cool.game.py
+++
@@ -1,7 +0,0 @@
-int c = cool
-int j = false
-for(int i = 0; i < 100; i++){
-  print this
-}
-end
-#comment
--- testing/target/cool.game.py
+++
@@ -1,6 +0,0 @@
-int c = false
-int j = cool
-for(int i = 0; i < 100; i++){
-}
-end
-#comment

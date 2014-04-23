3f7849b2e4d93271c3d5ac8add9bb4a361128a67
James Wen <jrw2175@columbia.edu>
2014-04-23 00:07:01 -0400

    Added LoopTest test to suite and altered test script to consider missing correct comparison file as failed test

--- testing/test.py
+++ testing/test.py
@@ -3,6 +3,9 @@ import os
 import time
 
 def test():
+
+  print("")
+
   #System commands to compile test files
   subprocess.Popen("./gamec ./source/*.game && mv ./source/*.game.py ./target;", shell=True)
   time.sleep(1)
@@ -13,11 +16,11 @@ def test():
   time.sleep(0.3)
 
   num_failed = 0
-  print("====================================================")
+  print("\n====================================================\n")
   for file_name in os.listdir("./source"):
     if ".game" in file_name: num_failed += compare_output(file_name)
   print("{} Test Case(s) Failed\n".format(num_failed))
-  print("====================================================")
+  print("====================================================\n")
 
 def run_case(file_name):
   #run target/python file and pipe output into .txt file in output directory
@@ -29,7 +32,11 @@ def run_case(file_name):
 
 def compare_output(file_name):
   #if diff is not 0, print diff and then print expected vs. actual
-  if os.stat("./diffs/{}.txt.diff".format(file_name)).st_size == 0:
+  if "{}.txt".format(file_name) not in os.listdir("./correct"):
+    print("{} Test Case failed.".format(file_name))
+    print("No correct output file for {} Test Case exists.\n".format(file_name))
+    return 1
+  elif os.stat("./diffs/{}.txt.diff".format(file_name)).st_size == 0:
     print("{} Test Case passed.\n".format(file_name))
     return 0
   else:

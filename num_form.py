db0088e5f3f77391183705654420c25b763f9cf5
Will Mcauliff <womcauliff@gmail.com>
2014-04-27 21:06:33 -0400

    debugged num_form()

--- num_form.py
+++ num_form.py
@@ -1,21 +1,14 @@
 import math
 
-function num_form(format, number) {
-	if (isinstance(format, basestring) && isinstance(number, (int, long, float, complex)) {
-		if (format == "#") {
+def num_form(format, number):
+	if (isinstance(format, basestring) and isinstance(number, (int, long, float, complex))):
+		if (format == "#"):
 			return str(math.floor(number))
-		}
-		else {
-
-		}
-		pieces = format.split('.', 1)
-		decimalplaces = pieces[1]
-		printformat = "%" + "1." + str(len(decimalplaces)) + "f"
-		return printformat % number 
-	}
-	else {
-		msg = (format + " must be of type text" 
-			+ " and " + number + " must be of type num")
+		else:
+			pieces = format.split('.', 1)
+			decimalplaces = pieces[1]
+			printformat = "%" + "1." + str(len(decimalplaces)) + "f"
+			return printformat % number
+	else:
+		msg = (format + " must be of type text" + " and " + number + " must be of type num")
 		raise TypeError(msg)
-	}
-}

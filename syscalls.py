d49735354933bb87e6e45a8fdc7695ac65c40685
Dylan Hicks <dth2115@columbia.edu>
2014-05-07 16:11:42 -0400

    added load instruction

--- syscalls.py
+++ syscalls.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import math
 import json
+import sys
 def num_form(format, number):
 	if (format == "#"):
 		return str(math.floor(number))
@@ -14,6 +15,51 @@ type_boolean = False
 type_str = "test"
 type_float = 10.0
 type_list = []
+
+def list_check(x):
+    if len(x) > 5 and x[:5]=="list(":
+        return x[5:-1]
+    else:
+        return None
+
+def load_function(in_class,types,file_name):
+        f = open(file_name,'r')
+        data = json.loads(f.read())
+        data = data.get("GAME",None)
+        if data==None:
+                print 'Invalid GAME data file.'
+                exit(0)
+        f.close()
+        return load_recurse(in_class,types,data)
+
+def load_recurse(in_class,types,data):
+        temp1 = list_check(types)
+        if temp1 == None:
+                temp2 = scan_classes.get(types,"")
+                if temp2 != "":
+                        for x in temp2["members"]:
+                                setattr(in_class,x,load_recurse(getattr(in_class,x),temp2["members"][x],data[x]))
+                else:
+                        if types == "text" and isinstance(data,basestring):
+                                return data
+                        elif types == "num" and isinstance(data,float):
+                                return data
+                        elif types == "bool" and isinstance(data,bool):
+                                return data
+                        else:
+                                print 'Invalid GAME data file.'
+                                exit(0)
+        else:
+                temp11 = [ ]
+                temp2 = scan_classes.get(temp1,"")
+                for x in data:
+                        if temp2 == "":
+                                temp11.append(load_recurse(None,temp1,x))
+                        else:
+                                new1 = globals()[temp1]
+                                temp11.append(load_recurse(new1,temp1,x))
+                return temp11
+        return in_class
 
 #ele is the string of the attribute
 #attr is the actual attribute

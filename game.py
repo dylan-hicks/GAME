72e74cfb66d537e569b9d594fce0ff6c9194dcb6
Dylan Hicks <dth2115@columbia.edu>
2014-05-07 19:24:47 -0400

    fixed import syscall

--- game.py
+++ game.py
@@ -246,7 +246,7 @@ names = { }
 def p_program_lines(t):
     '''program_lines : include_lines lines'''
     sys_calls = open("syscalls.py",'r')
-    toWrite = sys_calls.read()+"\nscan_classes = "+json.dumps(scan_classes)+"\n"+t[2]+"main()"
+    toWrite = sys_calls.read()+"\nscan_classes = "+json.dumps(scan_classes)+"\n"+t[2]+"if __name__ == '__main__': main()"
     toWrite = re.sub(r"\n[\t\n ]*\n","\n",toWrite) #removes excess lines
     out_file = open("{}.py".format(sys.argv[1]).replace(".temp", ""), "w")
     out_file.write(toWrite)

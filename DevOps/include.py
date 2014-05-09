13879ba269e649aa76a69941ecae268f0ec0d310
James Wen <jrw2175@columbia.edu>
2014-05-09 15:02:50 -0400

    Fixed include.py to allow recursive directory searching for include statements

--- DevOps/include.py
+++ DevOps/include.py
@@ -5,44 +5,42 @@ import os
 included = []
 
 #Returns what a file should look like after processing all include statements and swapping these out with the appropriate library code
-def include(file_name):
+def include(file_name, directory):
   file = open(file_name, "r+")
   contents = file.readlines()
   #Screen for include statements
   i = 0
   for line in contents:
-    regMatch = re.match(r'[\s]*include[\s]*["][\s]*(stdlib/)?([a-zA-Z_-]*)(.game)?[\s]*["][\s]*', line, flags=0)
+    regMatch = re.match(r'[\s]*include[\s]*["][\s]*([a-zA-Z_-]*/)?([a-zA-Z_-]*)(.game)?[\s]*["][\s]*', line, flags=0)
     if regMatch: #include statement match
-      if regMatch.group(2) not in included:
-        #get library file name
-        library_file_name = "{}.game".format(regMatch.group(2)).lower()
+      #construct relative library file path name
+      prefix = regMatch.group(1)
+      if prefix == None:
+        prefix = ""
+      unique = "./{}{}{}.game".format(directory, prefix, regMatch.group(2)).lower()
+      if unique not in included:
         #check for existence of library file
-        if file_exists(library_file_name):
+        if os.path.isfile(unique):
           #add library name to included array
-          included.append(regMatch.group(1))
+          included.append(unique)
           #recursively call include on library file to get prepared content
-          to_append = include(library_file_name)
+          to_append = include(unique, regMatch.group(1))
           #add file contents of prepared library file to our contents and remove include line
           contents[i] = ""
           contents [i:i] = to_append
           #contents = contents + to_append
         else:
-          print "{} does not exist\n".format(library_file_name)
-          sys.exit()
+          print "{} does not exist\n".format(unique)
+          sys.exit(1)
       else:
         contents.remove(line)
     i = i + 1;
   file.close()
   return contents
 
-def file_exists(file_name):
-  if file_name in os.listdir("./"):
-    return True
-  return False
-
 if __name__ == '__main__':
   name = sys.argv[1]
   #create new temp file with .temp file extension
   file = open("{}.temp".format(sys.argv[1]), 'w')
-  for line in include(name):
+  for line in include(name, ""):
     file.write("%s" % line)
